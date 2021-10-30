eadcfe2afa9e8432ea453d67da97f2cf3b1931e7
peresapy <marc.perellosapina@gmail.com>
2021-10-30 14:19:20 +0200

    Test changes

--- torandomlydo/test/widget_test.dart
+++ torandomlydo/test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:torandomdo/main.dart';
+import 'package:torandomlydo/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
